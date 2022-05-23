f619bb3a4bf305fe7ece5d7a81f425eb55e38af8
Ankit <mittalankit2404@gmail.com>
2022-05-23 15:39:14 +0530

    make changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,53 @@
 <template>
-  <hello-world />
+<v-app id="inspire">
+
+    <v-app-bar app class="amber lighten-1
+">
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+        <v-toolbar-title>
+            <v-icon large>
+                mdi-chevron-left
+            </v-icon>
+            Greetings
+        </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" fixed temporary>
+    </v-navigation-drawer>
+    <v-row justify="center" class="mt-1">
+        <v-expansion-panels accordion>
+            <v-expansion-panel>
+                <v-expansion-panel-header expand-icon="" class="text-decoration-underline font-weight-black subtitle-1">
+                    Creative Settings
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <div class="d-flex flex-wrap justify-space-between align-center">
+
+                    <v-switch :label="`Your Name`" v-model="switch1"></v-switch>
+                    <v-switch :label="`Your Number`" v-model="switch2"></v-switch>
+                    <v-switch :label="`Your Phone`" v-model="switch4"></v-switch>
+                    <v-switch :label="`Your Address`" v-model="switch3"></v-switch>
+                  </div>
+
+                </v-expansion-panel-content>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </v-row>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, repellendus quos, dolorem inventore fugit nulla blanditiis praesentium consectetur ullam officia, obcaecati sint dolores iure illo labore ipsa quidem assumenda ipsum?
+    
+</v-app>
+
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+export default {
+    data: () => ({
+        drawer: null,
+        switch1: true,
+        switch2: true,
+        switch3: true,
+        switch4: true,
+    }),
+}
 </script>
