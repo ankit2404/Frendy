2396c20d8b07e0d3cbc9bd5a6de2c67fdf2e32c9
Ankit <mittalankit2404@gmail.com>
2022-05-25 10:02:29 +0530

    connect backend

--- server/index.js
+++ server/index.js
@@ -2,11 +2,14 @@ const port = 8008;
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors')
 const db = require('./config/mongoose');
 const env = require('./config/environment');
 
+app.use(cors())
 app.use(express.urlencoded({extended:true}));
 app.use('/', require('./routes/index'));
+
 
 app.listen(port,function(error){
     if(error){
